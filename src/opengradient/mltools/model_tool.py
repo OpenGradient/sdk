0598cbd4ba4946ac95bfb2f748e7550c78e50fa9
Adam Balogh <adambalogh@Adams-Air.lan>
2025-01-02 17:56:26 -0500

    tool

--- src/opengradient/mltools/model_tool.py
+++ src/opengradient/mltools/model_tool.py
@@ -6,21 +6,26 @@ import opengradient as og
 
 def create_og_model_tool(
     model_cid: str, 
-    input_fetcher: Callable,
+    tool_name: str,
     tool_description: str, 
-    input_description: str,
-    inference_mode: og.InferenceMode) -> BaseTool:
+    input_getter: Callable,
+    inference_mode: og.InferenceMode= og.InferenceMode.VANILLA) -> BaseTool:
+    """
+    Returns a LangChain-compatible tool that invokes the given OpenGradient 
+    model and returns the result as output.
+    """
     # define runnable
     def model_executor():
         tx_hash, output = og.infer(
             model_cid=model_cid,
             inference_mode=inference_mode,
-            model_input=input_fetcher()
+            model_input=input_getter()
         )
 
         return output
 
-    return StructuredTool(
+    return StructuredTool.from_function(
         func=model_executor,
+        name=tool_name,
         description=tool_description
     )
