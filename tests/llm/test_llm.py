d67e9030ed381fe0061a14ab073c2c3967fa4bf2
kukac <adambalogh@users.noreply.github.com>
2025-01-16 11:58:19 -0500

    ruff format (#88)
    Co-authored-by: Adam Balogh <adambalogh@adams-air.mynetworksettings.com>

--- tests/llm/test_llm.py
+++ tests/llm/test_llm.py
@@ -5,17 +5,16 @@ from opengradient.llm import OpenGradientChatModel
 from langgraph.prebuilt import create_react_agent
 from langchain_core.tools import tool
 
+
 class TestLLM(unittest.TestCase):
     def setUp(self):
         """Set up test fixtures before each test method"""
-        private_key = os.environ.get('PRIVATE_KEY')
+        private_key = os.environ.get("PRIVATE_KEY")
         if not private_key:
             raise ValueError("PRIVATE_KEY environment variable is not set")
 
-        self.llm = OpenGradientChatModel(
-            private_key=private_key,
-            model_cid='meta-llama/Llama-3.1-70B-Instruct')
-    
+        self.llm = OpenGradientChatModel(private_key=private_key, model_cid="meta-llama/Llama-3.1-70B-Instruct")
+
     def test_simple_completion(self):
         message = self.llm.invoke("say 'hello'. literally")
         self.assertIn("hello", message.content)
@@ -27,14 +26,10 @@ class TestLLM(unittest.TestCase):
             return "Your balance is 0.145"
 
         agent_executor = create_react_agent(self.llm, [get_balance])
-        events = agent_executor.stream(
-            {"messages": [("user", "What is my balance?")]},
-            stream_mode="values",
-            debug=False
-        )
+        events = agent_executor.stream({"messages": [("user", "What is my balance?")]}, stream_mode="values", debug=False)
 
-        self.assertIn("0.145", list(events)[-1]['messages'][-1].content)
+        self.assertIn("0.145", list(events)[-1]["messages"][-1].content)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
