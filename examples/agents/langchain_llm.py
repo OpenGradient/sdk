928842ac4c49160f936856aacb58da770f7a80f6
Adam Balogh <adambalogh@Adams-MacBook-Air.local>
2025-02-25 21:24:25 -0700

    fix example

--- examples/agents/langchain_llm.py
+++ examples/agents/langchain_llm.py
@@ -1,13 +1,15 @@
 import os
 from langgraph.prebuilt import create_react_agent
+from langchain_core.tools import BaseTool
 import opengradient as og
+from typing import List
 
 llm = og.llm.langchain_adapter(private_key=os.environ.get("OG_PRIVATE_KEY"), model_cid=og.LLM.QWEN_2_5_72B_INSTRUCT)
 
 # You define tools here
-tools = []
+tools: List[BaseTool] = []
 
-agent = create_react_agent(llm=llm, tools=tools, prompt="You are a friendly agent...")
+agent = create_react_agent(model=llm, tools=tools, prompt="You are a friendly agent...")
 
 
 # Helper to print agent steps
