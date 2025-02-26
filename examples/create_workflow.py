c67b274115c0d72651422a181dfce863298248aa
Adam Balogh <adambalogh@Adams-MacBook-Air.local>
2025-02-25 21:02:19 -0700

    create workflow

--- examples/create_workflow.py
+++ examples/create_workflow.py
@@ -1,1 +1,39 @@
+import os
 import opengradient as og
+
+og_client = og.new_client(email=None, password=None, private_key=os.environ.get("OG_PRIVATE_KEY"))
+
+# Define model input
+input_query = og.HistoricalInputQuery(
+    base="ETH",
+    quote="USD",
+    total_candles=10,
+    candle_duration_in_mins=30,
+    order=og.CandleOrder.ASCENDING, 
+    candle_types=[
+        og.CandleType.OPEN,
+        og.CandleType.HIGH,
+        og.CandleType.LOW,
+        og.CandleType.CLOSE
+    ]
+)
+
+# Define schedule
+scheduler_params = og.SchedulerParams(
+    frequency=1800,
+    duration_hours=2
+)
+
+# Base model CID
+model_cid = "QmRhcpDXfYCKsimTmJYrAVM4Bbvck59Zb2onj3MHv9Kw5N"
+
+# Deploy schedule
+contract_address = og.new_workflow(
+    model_cid=model_cid,
+    input_query=input_query,
+    # Input name in ONNX model
+    input_tensor_name="open_high_low_close",
+    scheduler_params=scheduler_params
+)
+
+print(f"Deployed workflow at address: {contract_address}")
