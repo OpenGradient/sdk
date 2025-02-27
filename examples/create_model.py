61b21ee9e8bc270cb637f5e67e6b48a87bc33003
Adam Balogh <adambalogh@Adams-MacBook-Air.local>
2025-02-26 21:14:27 -0700

    rm

--- examples/create_model.py
+++
@@ -1,6 +0,0 @@
-import os
-import opengradient as og
-
-og_client = og.new_client(email=None, password=None, private_key=os.environ.get("OG_PRIVATE_KEY"))
-
-og_client.create_model(model_name="", model_desc="", version="1.0.0")
