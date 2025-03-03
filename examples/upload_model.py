c4410d3ba377b53aa467d741344b0547cae29cd2
Adam Balogh <adambalogh@adams-air.mynetworksettings.com>
2025-03-03 18:09:11 -0500

    upload

--- examples/upload_model.py
+++ examples/upload_model.py
@@ -7,14 +7,14 @@ og_client = og.new_client(
     password=os.environ.get("OG_MODEL_HUB_PASSWORD"), 
     private_key=os.environ.get("OG_PRIVATE_KEY"))
 
-model = og_client.create_model(
+model_repo = og_client.create_model(
     model_name="[Demo] Custom Model",
     model_desc="My custom model for demoing Model Hub"
 )
 
 result = og_client.upload(
-    model_name=model["name"],
-    version=model["versionString"],
+    model_name=model_repo["name"],
+    version=model_repo["versionString"],
     model_path="./path/to/model.onnx"
 )
 
