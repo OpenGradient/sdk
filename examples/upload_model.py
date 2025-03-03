266d06f4e0d72d5c6cece25e91844865e869eaeb
Adam Balogh <adambalogh@adams-air.mynetworksettings.com>
2025-03-03 18:08:52 -0500

    upload model

---
+++ examples/upload_model.py
@@ -0,0 +1,21 @@
+import os
+
+import opengradient as og
+
+og_client = og.new_client(
+    email=os.environ.get("OG_MODEL_HUB_EMAIL"), 
+    password=os.environ.get("OG_MODEL_HUB_PASSWORD"), 
+    private_key=os.environ.get("OG_PRIVATE_KEY"))
+
+model = og_client.create_model(
+    model_name="[Demo] Custom Model",
+    model_desc="My custom model for demoing Model Hub"
+)
+
+result = og_client.upload(
+    model_name=model["name"],
+    version=model["versionString"],
+    model_path="./path/to/model.onnx"
+)
+
+print(f"Uploaded model, use following CID to access: {result["modelCid"]}")
