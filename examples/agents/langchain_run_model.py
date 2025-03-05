c0288e778a168c134e6ee2d595c204800be59177
kylexqian <kylexqian@gmail.com>
2025-03-04 19:10:41 -0500

    Update the example agent files

--- examples/agents/langchain_run_model.py
+++ examples/agents/langchain_run_model.py
@@ -10,14 +10,36 @@ llm = og.llm.langchain_adapter(private_key=og_key, model_cid=og.LLM.QWEN_2_5_72B
 og.init(private_key=og_key, email=None, password=None)
 
 # Create ad-hoc model inference tool
+def model_input_provider():
+    # This function can be used to retrieve live data, process data, etc. 
+    return {
+            "open_high_low_close": [
+                [2535.79, 2535.79, 2505.37, 2515.36],
+                [2515.37, 2516.37, 2497.27, 2506.94],
+                [2506.94, 2515, 2506.35, 2508.77],
+                [2508.77, 2519, 2507.55, 2518.79],
+                [2518.79, 2522.1, 2513.79, 2517.92],
+                [2517.92, 2521.4, 2514.65, 2518.13],
+                [2518.13, 2525.4, 2517.2, 2522.6],
+                [2522.59, 2528.81, 2519.49, 2526.12],
+                [2526.12, 2530, 2524.11, 2529.99],
+                [2529.99, 2530.66, 2525.29, 2526],
+            ]
+        }
+
+def output_formatter(inference_result: og.InferenceResult):
+    # Format output of the inference to 3 decimal places.
+    return format(float(inference_result.model_output["Y"].item()), ".3%")
+
 run_model_tool = og.alphasense.create_run_model_tool(
     tool_type=og.alphasense.ToolType.LANGCHAIN,
-    model_cid="0x5DbC2b798501d08cF2957f005e25a5C34FAC2b1c",
-    tool_name="BtcSpotForecast",
-    tool_description="Returns the expected change of bitcoin price over the next 30 mins",
-    input_getter=lambda: [],
-    input_schema=None,
-    output_formatter=lambda x: f"Expected return: {x.numbers['regression_output'][0] * 100}%",
+    model_cid="QmRhcpDXfYCKsimTmJYrAVM4Bbvck59Zb2onj3MHv9Kw5N",
+    tool_name="OneHourEthUsdtVolatilityForecaster",
+    model_input_provider=model_input_provider,
+    model_output_formatter=output_formatter,
+    tool_input_schema=None,  # If the model_input_provider does not require any input arguments, then tool_input_schema is not required
+    tool_description="Returns the predicted price volatility for the trading pair ETH/USDT over the next hour",
+    inference_mode=og.InferenceMode.VANILLA
 )
 
 tools: List[BaseTool] = [run_model_tool]
@@ -34,5 +56,5 @@ def print_stream(stream):
             message.pretty_print()
 
 
-inputs = {"messages": [("user", "what is the price of BTC going to be?")]}
+inputs = {"messages": [("user", "what is the future volatility of the trading pair ETH/USDT?")]}
 print_stream(agent.stream(inputs, stream_mode="values"))
