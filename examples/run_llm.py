92a28a6f4d4ef0850abc360eb9f47c9b3bbca876
Kyle Qian <kylexqian@gmail.com>
2025-12-03 18:00:44 +0900

    Update run_llm.py

--- examples/run_llm.py
+++ examples/run_llm.py
@@ -11,3 +11,34 @@ completion = og_client.llm_chat(
 
 print(f"Response: {completion.chat_output['content']}")
 print(f"Tx hash: {completion.transaction_hash}")
+
+## Run non-hosted LLMs through OpenGradient routers
+# Use opengradient version pip install opengradient==0.5.0a1
+# This works for Gemini, Anthropic, and OpenAI models in langchain
+# https://docs.langchain.com/oss/python/integrations/chat
+from opengradient import DEFAULT_INFERENCE_CONTRACT_ADDRESS, DEFAULT_RPC_URL, DEFAULT_API_URL
+og_gemini_client = og.Client(
+    private_key=os.environ.get("OG_PRIVATE_KEY"),
+    rpc_url=DEFAULT_RPC_URL,
+    api_url=DEFAULT_API_URL,
+    contract_address=DEFAULT_INFERENCE_CONTRACT_ADDRESS,
+    google_api_key=os.environ.get("GOOGLE_API_KEY")
+)
+
+result = og_gemini_client.llm_completion(
+    model_cid="gemini-2.5-flash-lite", 
+    inference_mode=LlmInferenceMode.VANILLA, 
+    prompt="Write a haiku about coding")
+
+print(f"Response: {result.completion_output}")
+print(f"Tx hash: {completion.transaction_hash}")
+
+messages = [
+    {"role": "user", "content": "What is Python?"}, 
+    {"role": "assistant", "content": "Python is a high-level programming language."},
+    {"role": "user", "content": "What makes it good for beginners?"}
+]
+
+result = client.llm_chat(model_cid="gemini-2.5-flash-lite", inference_mode=LlmInferenceMode.VANILLA, messages=messages)
+print(f"Response: {completion.chat_output['content']}")
+print(f"Tx hash: {completion.transaction_hash}")
