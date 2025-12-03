8436bc744aed7826df8237d456802e710c466e62
Kyle Qian <kylexqian@gmail.com>
2025-12-03 19:42:26 +0900

    Update run_llm.py

--- examples/run_llm.py
+++ examples/run_llm.py
@@ -13,22 +13,22 @@ print(f"Response: {completion.chat_output['content']}")
 print(f"Tx hash: {completion.transaction_hash}")
 
 ## Run non-hosted LLMs through OpenGradient routers
-# Use opengradient version pip install opengradient==0.5.0a1
+# Use opengradient version `pip install opengradient==0.5.0a2`
 # This works for Gemini, Anthropic, and OpenAI models in langchain
 # https://docs.langchain.com/oss/python/integrations/chat
 from opengradient import DEFAULT_INFERENCE_CONTRACT_ADDRESS, DEFAULT_RPC_URL, DEFAULT_API_URL
-og_gemini_client = og.Client(
+og_gemini_client = og.new_client(
+    email=None, 
+    password=None, 
     private_key=os.environ.get("OG_PRIVATE_KEY"),
-    rpc_url=DEFAULT_RPC_URL,
-    api_url=DEFAULT_API_URL,
-    contract_address=DEFAULT_INFERENCE_CONTRACT_ADDRESS,
     google_api_key=os.environ.get("GOOGLE_API_KEY")
 )
 
 result = og_gemini_client.llm_completion(
     model_cid="gemini-2.5-flash-lite", 
-    inference_mode=LlmInferenceMode.VANILLA, 
-    prompt="Write a haiku about coding")
+    inference_mode=og.LlmInferenceMode.VANILLA, 
+    prompt="Write a haiku about coding"
+)
 
 print(f"Response: {result.completion_output}")
 print(f"Tx hash: {completion.transaction_hash}")
@@ -39,6 +39,11 @@ messages = [
     {"role": "user", "content": "What makes it good for beginners?"}
 ]
 
-result = client.llm_chat(model_cid="gemini-2.5-flash-lite", inference_mode=LlmInferenceMode.VANILLA, messages=messages)
+result = client.llm_chat(
+    model_cid="gemini-2.5-flash-lite", 
+    inference_mode=og.LlmInferenceMode.VANILLA, 
+    messages=messages
+)
+
 print(f"Response: {completion.chat_output['content']}")
 print(f"Tx hash: {completion.transaction_hash}")
