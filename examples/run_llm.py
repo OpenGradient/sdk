e14b34a59aa324ca50701dc6e57ac005ff56abd6
kylexqian <kylexqian@gmail.com>
2026-01-08 04:14:06 -0800

    Remove local models and fix examples

--- examples/run_llm.py
+++ examples/run_llm.py
@@ -4,7 +4,7 @@ import opengradient as og
 og_client = og.new_client(email=None, password=None, private_key=os.environ.get("OG_PRIVATE_KEY"))
 
 completion = og_client.llm_chat(
-    model_cid=og.LLM.DOBBY_UNHINGED_3_1_8B,
+    model_cid=og.LLM.GPT_4O,
     messages=[{"role": "user", "content": "say something funny"}],
     inference_mode=og.InferenceMode.VANILLA,
 )
