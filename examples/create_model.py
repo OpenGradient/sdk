5315bc3e6b844e719b7d75736ecfda26535753ea
balogh.adam@icloud.com <adambalogh@Adams-MacBook-Pro.local>
2026-01-08 13:42:15 +0000

    example

--- examples/create_model.py
+++ examples/create_model.py
@@ -3,4 +3,8 @@ import opengradient as og
 
 og_client = og.new_client(email=None, password=None, private_key=os.environ.get("OG_PRIVATE_KEY"))
 
-og_client.create_model(model_name="", model_desc="", version="1.0.0")
+og_client.create_model(
+    model_name="example-model",
+    model_desc="An example machine learning model for demonstration purposes",
+    version="1.0.0"
+)
