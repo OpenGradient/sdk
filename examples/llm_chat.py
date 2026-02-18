55f0444d61591047e2d2e87f662c68bdf7a7f6d9
balogh.adam@icloud.com <adambalogh@Adams-MacBook-Pro.local>
2026-02-18 21:35:10 +0100

    dont print permit

--- examples/llm_chat.py
+++ examples/llm_chat.py
@@ -5,9 +5,7 @@ import opengradient as og
 client = og.Client(
     private_key=os.environ.get("OG_PRIVATE_KEY"),
 )
-
-approval = client.llm.ensure_opg_approval(opg_amount=5)
-print(f"$OPG Approval Status: {approval}")
+client.llm.ensure_opg_approval(opg_amount=5)
 
 messages = [
     {"role": "user", "content": "What is the capital of France?"},
--- examples/llm_chat_streaming.py
+++ examples/llm_chat_streaming.py
@@ -5,7 +5,7 @@ import opengradient as og
 client = og.Client(
     private_key=os.environ.get("OG_PRIVATE_KEY"),
 )
-print(f"$OPG Approval status: {client.llm.ensure_opg_approval(opg_amount=1)}")
+client.llm.ensure_opg_approval(opg_amount=1)
 
 messages = [
     {"role": "user", "content": "What is Python?"},
